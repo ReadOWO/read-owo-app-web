16a9396d5f0ba859bb6c06a293e331e97b243ecd
jhuan <j.huanca4141@gmail.com>
2023-06-24 10:04:12 -0500

    Feat: modify profile

--- src/authentication/components/profile.component.vue
+++ src/authentication/components/profile.component.vue
@@ -1,8 +1,9 @@
 <template>
     <div>
         <img src="../public/images/fontProfile.png" alt="ProfileImage"/>
-        <p>Nombre: {{ user.userName }}</p>
-        <p>Email: {{ user.email }}</p>
+        <p>Nombre: {{ profile.name }}</p>
+        <p>Foto de Perfil:</p><img :src="profile.urlPhoto" :alt="profile.name">
+        <p>Email: {{ profile.user.email}}</p>
     </div>
 </template>
 <script>
@@ -10,7 +11,7 @@ import { mapState } from 'vuex'
 export default {
     name: "profile",
     computed: {
-        ...mapState(['user'])
+        ...mapState(['profile'])
     }
 }
 </script>
