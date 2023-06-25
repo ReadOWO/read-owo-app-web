65c8112f82e6d7d3fad275fbba43ddca83bc858f
MartinCifuentes <cccifuentes.martin@gmail.com>
2023-06-24 20:57:16 -0500

    feat: Updated paths

--- src/publish/services/genre-service.js
+++ src/publish/services/genre-service.js
@@ -2,6 +2,6 @@ import {HttpService} from "@/shared/services/base-service";
 export class GenreService extends HttpService {
     constructor() {
         super();
-        this.baseGet = 'genres';
+        this.baseGet = 'genre';
     }
 }
