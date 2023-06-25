c6980ed601e98453840dd5cce1c10b33501de6eb
MartinCifuentes <cccifuentes.martin@gmail.com>
2023-06-24 20:43:30 -0500

    feat: Updated book-service

--- src/publish/services/book-service.js
+++ src/publish/services/book-service.js
@@ -2,6 +2,6 @@ import {HttpService} from "@/shared/services/base-service";
 export class BookService extends HttpService {
     constructor() {
         super();
-        this.baseGet = 'books';
+        this.baseGet = 'book';
     }
 }
