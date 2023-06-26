b8cd296a366a6ddac891f1c884f4a6d2a3981656
MartinCifuentes <cccifuentes.martin@gmail.com>
2023-06-26 17:09:38 -0500

    update sidebar

--- src/shared/components/side-menu.component.vue
+++ src/shared/components/side-menu.component.vue
@@ -1,24 +1,29 @@
 <template>
-    <pv-sidebar :visible="visible" v-on:update:visible="updateVisible">
-        <div class="menu-container">
-            <ul class="menu-list">
-                <li @click="closesidebar" class="menu-item"> <i class="pi pi-bars"></i> RETURN</li>
-                <input
-                        type="search"
-                        class="search-input"
-                        placeholder="Search..."
-                        @input="onSearchInput"
-                />
-                <router-link to="/creation"><pv-button class="menu-item">Creations</pv-button></router-link>
-                <router-link to="/publish"><pv-button class="menu-item">Publish your work</pv-button></router-link>
-                <router-link to="/login"><pv-button class="menu-item">Authors</pv-button></router-link>
-            </ul>
-            <ul class="menu-list">
-                <li class="menu-item">Configuration</li>
-                <li class="menu-item">Found an error? Let us know</li>
-            </ul>
-        </div>
-    </pv-sidebar>
+  <pv-sidebar :visible="visible" v-on:update:visible="updateVisible">
+    <div class="menu-container">
+      <ul class="menu-list">
+        <li @click="closesidebar" class="menu-item">
+          <i class="pi pi-bars"></i> RETURN
+        </li>
+        <input
+            type="search"
+            class="search-input"
+            placeholder="Search..."
+            @input="onSearchInput"
+        />
+        <router-link to="/creation" class="menu-item">
+          <pv-button>Creations</pv-button>
+        </router-link>
+        <router-link to="/publish" class="menu-item">
+          <pv-button>Your Work</pv-button>
+        </router-link>
+      </ul>
+      <ul class="menu-list">
+        <li class="menu-item">Configuration</li>
+        <li class="menu-item">Found an error? Let us know</li>
+      </ul>
+    </div>
+  </pv-sidebar>
 </template>
 
 <script>
@@ -41,39 +46,40 @@ export default {
 </script>
 
 <style scoped>
-
 .menu-container {
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 100%;
 }
 
 .menu-list {
-    list-style: none;
-    padding: 0;
-    margin: 0;
+  list-style: none;
+  padding: 0;
+  margin: 0;
 }
 
 .menu-item {
-    cursor: pointer;
-    padding: 12px 24px;
-    font-size: 18px;
-    font-family: "Arial", sans-serif;
-    line-height: 1.6;
-    transition: background-color 0.3s;
+  cursor: pointer;
+  padding: 12px 24px;
+  font-size: 18px;
+  font-family: "Arial", sans-serif;
+  line-height: 1.6;
+  transition: background-color 0.3s;
+  margin-bottom: 10px;
 }
 
 .menu-item:hover {
-    background-color: #e0e0e0;
+  background-color: #e0e0e0;
 }
+
 .search-input {
-    display: block;
-    width: 100%;
-    padding: 12px 16px; /* Aumenta el padding para hacer el input más grande */
-    margin-top: 12px;
-    font-size: 18px; /* Aumenta el tamaño de fuente */
-    border: 1px solid #ccc;
-    border-radius: 4px;
+  display: block;
+  width: 100%;
+  padding: 12px 16px;
+  margin-top: 12px;
+  font-size: 18px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
 }
 </style>
