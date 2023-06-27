7728e0dddb80cbe1b63794b12c4620994e45ccd5
MartinCifuentes <cccifuentes.martin@gmail.com>
2023-06-27 16:12:56 -0500

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/read-owo-app-web/',
     plugins: [vue()],
     resolve: {
         alias: {
