b1cb9d3ae2dd51be8f2fb8fdf679800ad6e2c248
MartinCifuentes <cccifuentes.martin@gmail.com>
2023-06-27 15:38:46 -0500

    updated your-artwork

--- src/publish/components/your-artwork.component.vue
+++ src/publish/components/your-artwork.component.vue
@@ -1,29 +1,33 @@
 <template>
     <div>
-        <h1 class="text-center">Every story begins with a single WORD</h1>
-        <h3>Your Work</h3>
-        <table>
-            <thead>
-            <tr>
-                <th>PHOTO</th>
-                <th>TITLE</th>
-                <th>NUMBER OF CHAPTERS</th>
-                <th>PUBLICATION DATE</th>
-                <TH>ACTIONS</TH>
-            </tr>
-            </thead>
-            <tbody>
-            <tr v-for="(book, index) in publishedBooks" :key="index">
-                <td><img class="img-container" :src="book.booksImage" :alt="book.booksTitle" @click="showBookInfo(book)"></td>
-                <td>{{ book.booksTitle }}</td>
-                <td>{{ book.chaptersNumber }}</td>
-                <td>{{ book.booksDate }}</td>
-                <td>
-                    <!-- Aquí puedes agregar botones para editar o eliminar la publicación -->
-                </td>
-            </tr>
-            </tbody>
-        </table>
+      <h1 class="text-center">Every story begins with a single WORD</h1>
+      <h3>Your Work</h3>
+      <button @click="saveBook" class="p-button p-button-rounded p-button-success">PUBLISH</button>
+      <table>
+        <thead>
+          <tr>
+            <th>PHOTO</th>
+            <th>TITLE</th>
+            <th>NUMBER OF CHAPTERS</th>
+            <th>PUBLICATION DATE</th>
+            <TH>ACTIONS</TH>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(book, index) in publishedBooks" :key="index">
+            <td><img class="img-container" :src="book.booksImage" :alt="book.booksTitle" @click="showBookInfo(book)"></td>
+            <td>{{ book.booksTitle }}</td>
+            <td>{{ book.chaptersNumber }}</td>
+            <td>{{ book.booksDate }}</td>
+            <td>
+              <pv-button
+                  icon="pi pi-trash" class="p-button-text p-button-rounded"
+                  @click="deleteBook(book)"
+              />
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
 </template>
 <script>
@@ -38,6 +42,7 @@ export default {
         return {
             publishedBooks: [],
             book:{
+              booksId: "",
                 booksImage: "",
                 booksTitle: "",
                 booksDate: "",
@@ -57,6 +62,7 @@ export default {
             for (let bookAux of responseBook.data) {
                 if (this.profile.id === bookAux.profileId) {
                     this.book = {
+                        booksId : bookAux.id,
                         booksImage: bookAux.thumbnailUrl,
                         booksTitle: bookAux.title,
                         booksDate: bookAux.publishedAt,
@@ -72,11 +78,23 @@ export default {
             }
         },
         showBookInfo(book) {
-            // Aquí puedes agregar el código para mostrar la información del libro
+          this.$router.push({ name: 'book-item', params: { id: book.id } });
+        },
+        async deleteBook(book){
+          const bookService = new BookService();
+          await bookService.delete(book.booksId);
+          const index = this.publishedBooks.findIndex(b => b.booksId === book.booksId);
+          if (index !== -1) {
+            this.publishedBooks.splice(index, 1);
+          }
+        },
+        saveBook(){
+            this.$router.push({name: 'create'});
         }
     }
 }
 </script>
+
 <style scoped>
 .text-center {
     text-align: center;
