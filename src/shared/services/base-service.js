797bc6787dec7fad3c0c5a6d1324b38f6448f359
MartinCifuentes <cccifuentes.martin@gmail.com>
2023-06-28 11:31:03 -0500

    update path

--- src/shared/services/base-service.js
+++ src/shared/services/base-service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const http = axios.create({
-    baseURL: 'http://mangalounge-001-site1.itempurl.com/api/v1/',
+    baseURL: 'http://readowo-001-site1.htempurl.com/api/v1',
     header: {}
 });
 
